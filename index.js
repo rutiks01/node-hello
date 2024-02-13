a9b0cd07ad1c2f9b58796497f7ebb64c54e415df
Rutik Suryawanshi <144602268+rutiks01@users.noreply.github.com>
2024-02-13 19:48:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = 'Hello Node!\n';
   res.end(msg);
 });
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${port}/`);
 });
